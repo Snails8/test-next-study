965ce952b976fbc9d44a517b46ac35252e781ff6
S-tanishi <tanichi.u.n.20@gmail.com>
2021-04-20 14:38:03 +0900

    一覧ページの骨格作成

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import Layout from '../layouts/Layout'
+import React from "react";
+import {AlertLink} from "bootstrap-react";
+
+// function Index(props) {
+//     return <h1>Hello, {props.name}</h1>
+// }
+type Props = {
+    name: string
+}
+
+const IndexPage = ({ name }: Props) => {
+    return (
+        <React.Fragment>
+            <Layout>
+                <h1>Hey Next.js </h1>
+                <p>
+                    <Link>
+                        <a>about</a>
+                    </Link>
+                </p>
+            </Layout>
+        </React.Fragment>
+    )
+}
