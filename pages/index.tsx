52180889299032360af39a83785c6b6415ef0aec
S-tanishi <tanichi.u.n.20@gmail.com>
2021-04-23 17:54:20 +0900

    serverSidePropsで取得

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,42 @@
 import React from "react";
-import {awaitExpression, returnStatement} from "@babel/types";
+import Link from 'next/link'
 
-const Home = ({ stars }) => {
+const Home = ({ stars }:Props) => {
     return (
-        <div> Next stars: {stars} </div>
+        <div>
+            <p>Next stars: {stars}</p>
+            <Link href={"/get-server-side-props"}>
+                <a>Go to getServerSideProps</a>
+            </Link>
+        </div>
     )
 }
 
-Home.getInitialProps = async ({ req}) => {
-    const res = await fetch('https://api.github.com/repos/zeit/next.js')
-    const json = await res.json()
-    return {stars: json.stargazers_count}
-}
-
-// export async function getServerSideProps() {
+// Next.js 9.3囲碁の場合、getStaticPropsのgetServerSidepropsを使うべし
+// Home.getInitialProps = async ({  }) => {
 //     const res = await fetch('https://api.github.com/repos/zeit/next.js')
 //     const json = await res.json()
 //     return {stars: json.stargazers_count}
 // }
+
+// ビルド時に取得する
+// export async function getStaticProps() {
+//     const res = await fetch('https://api.github.com/repos/zeit/next.js')
+//     const json = await res.json()
+//     return {stars:json.stargazers_count}
+//
+// }
+
+// リクエスト時に取得
+export async function getServerSideProps() {
+    const res = await fetch('https://api.github.com/repos/zeit/next.js')
+    const json = await res.json()
+
+    return {
+        props: {
+            stars:json.stargazers_count,
+        },
+    }
+}
 
 export default Home
